001a5ff35a398db4ee56177960207d140e3d6b19
JustinJamesB <85745345+JustinJamesB@users.noreply.github.com>
2021-10-25 09:40:30 +0800

    Update alioss.js
    将服务器id 密钥等删除，防止泄露。

--- vue/vue-manage-system-master/src/utils/alioss.js
+++ vue/vue-manage-system-master/src/utils/alioss.js
@@ -2,8 +2,8 @@ var OSS = require('ali-oss');
 export function client() {
     var client = new OSS({
         region: 'oss-cn-hongkong',
-        accessKeyId: 'LTAI4GDRuer7CbcUjncN1xa2',
-        accessKeySecret: 'rrHlPjKp7PigsDLqfiXsLCAnH6P9IH',
+        accessKeyId: '',
+        accessKeySecret: '',
         bucket: 'minshopimage'
     }) //后端提供数据
     return client
